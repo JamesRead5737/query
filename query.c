c1dc423df713463b82d5961461c0b960271ac9fb
JamesRead5737 <42124229+JamesRead5737@users.noreply.github.com>
2020-06-18 09:37:43 +0100

    alpha

---
+++ query.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main (void)
+{
+	char *input;
+
+	printf("Content-Type:text/html;charset=utf8\n");
+	printf("<html>\n");
+	printf("<head>\n");
+	printf("<title>WoT ToT</title>\n");
+	printf("</head>\n");
+	printf("<body>\n");
+	printf("<form action=\"http://www.wottot.com/cgi-bin/query\">i\n");
+	printf("<div><input name=\"n\" size=100%></div>\n");
+	printf("<div><input type=\"submit\" value=\"Search\"></div>\n");
+	printf("</form>\n");
+	printf("</body>\n");
+	printf("</html>\n");
+}
